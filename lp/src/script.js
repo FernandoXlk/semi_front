90e6ba355cad19de0e34fd1b07bb34cd3c7a66d4
FernandoXlk <fernando2022norato@gmail.com>
2026-06-03 15:51:33 -0300

    Add files via upload

--- lp/src/script.js
+++ lp/src/script.js
@@ -4,3 +4,11 @@ function navTo(id){
         block: 'start'
     })
 }
+
+function enviar(){
+    const nome = document.getElementById('nome').value
+    const assunto = document.getElementById('assunto')
+    const mensagem = `staria de entrar em contato!\n\nNome: ${nome} \nAssunto: ${assunto}`
+    const msg = encodeURIComponent(mensagem)
+    window.open(`https://wa.me/554199999999?text=${msg}`)
+}
